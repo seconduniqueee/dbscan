cc6961d4678169de28de0b61985e883a1a552646
seconduniQueee <plotnikovfw@gmail.com>
2024-11-28 01:09:43 +0200

    Feat: implement basic, unefficient db scan

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-const MAX_POINTS = 60;
-const THRESHOLD = 80; // minPts?
+const MAX_POINTS = 100;
+const THRESHOLD = 20; // minPts?
+const MIN_NEIGHBORS_REQURIED = 1;
 const COLORS = [
   "#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF", 
   "#800000", "#808000", "#008000", "#000080", "#800080", "#808080", 
   "#C0C0C0", "#FFD700", "#FF6347", "#98FB98", "#FF1493", "#FF4500", 
   "#2E8B57", "#4B0082", "#D2691E", "#ADFF2F", "#A52A2A", "#F4A300", 
-  "#C71585", "#B0E0E6", "#A9A9A9", "#000000", "#FFFFFF", "#8B0000"
+  "#C71585", "#B0E0E6", "#A9A9A9", "#000000", "#FFFFFF", "#8B0000",
 ];
 
 let grid = document.querySelector(".grid");
 let points = generateSetOfPoints();
-let clusters = generateClusters(points);
+let { clusters, noise } = dbScan(points, THRESHOLD, MIN_NEIGHBORS_REQURIED);
 
-drawClusters(clusters);
-
+drawClusters(clusters, noise);
 
 function generateClusters(points) {
   let clusters = [];
@@ -75,13 +75,15 @@ function generateSetOfPoints() {
   return result;
 }
 
-function drawClusters(clusters) {
+function drawClusters(clusters, noise) {
   clusters.forEach((cluster, index) => {
     let clusterColor = COLORS[index];
 
-    drawClusterOutline(cluster);
+    // drawClusterOutline(cluster);
     cluster.forEach((p) => renderPoint(p, clusterColor))
   });
+
+  noise.forEach((p) => renderPoint(p, "#fff"));
 }
 
 function renderPoint({ x, y }, color) {
@@ -130,4 +132,67 @@ function calcDistance(point1, point2) {
   let dy = point2.y - point1.y;
 
   return Math.sqrt(dx**2 + dy**2);
+}
+
+// DB Scan
+function getPointKey(point) {
+  return `${point.x},${point.y}`;
+}
+
+function inHashMap(hm, point) {
+  return hm[getPointKey(point)];
+}
+
+function addToHashMap(hm, point, value) {
+  hm[getPointKey(point)] = value || true;
+}
+
+function dbScan(points, threshold, neighborsRequired) {
+  let clusters = [];
+  let corePointsHM = getCorePoints(points, threshold, neighborsRequired);
+  let corePointsArr = points.filter((p) => inHashMap(corePointsHM, p));
+  let noise = points.filter((p) => !inHashMap(corePointsHM, p));
+  let processed = {};
+
+  for (let corePoint of corePointsArr) {
+    if (inHashMap(processed, corePoint)) continue;
+
+    let cluster = [];
+    let candidates = [corePoint];
+
+    while(candidates.length) {
+      let candidate = candidates.pop();
+      let neighbors = inHashMap(corePointsHM, candidate);
+
+      cluster.push(candidate);
+      addToHashMap(processed, candidate);
+
+      neighbors
+        ?.filter((nPoint) => inHashMap(corePointsHM, nPoint) && !inHashMap(processed, nPoint))
+        ?.forEach((point) => candidates.push(point));
+    }
+
+    clusters.push(cluster);
+  }
+
+  return {clusters, noise };
+}
+
+function getCorePoints(points, threshold, neighborsRequired) {
+  let corePoints = {};
+  let processed = {};
+
+  for (let point of points) {
+    if (inHashMap(processed, point)) continue;
+
+    let neighbors = points.filter((p) => calcDistance(p, point) < threshold && p !== point);
+
+    if (neighbors.length >= neighborsRequired) {
+      addToHashMap(corePoints, point, neighbors);      
+    }
+
+    addToHashMap(processed, point);
+  }
+
+  return corePoints;
 }
